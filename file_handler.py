dc554ed3d1c7bc54f1033c237ff04bb99bf6d86b
SamuelSilva2310 <samuelsilva2310@gmail.com>
2021-01-14 09:51:15 +0000

    Basic App all Tasks can be stores and basic Functionallity

--- file_handler.py
+++ file_handler.py
@@ -6,19 +6,46 @@
 # task: the task the user wants to store
 
 def create_task(filename: str, task: str): 
-	with open("data/" + filename + ".txt", "a+") as f: 
-		f.write(task + "\n")
-
-def show_tasks(filename: str):
-	with open("data/" + filename + ".txt", "r") as f: 
-		lines = f.readlines()
-		for line in lines:
-			print(line)
+    with open("data/" + filename + ".txt", "a+") as f: 
+        f.write(task + "\n")
 
 
+#This method Shows  tasks, it will read all lines and print them, arguments:
+# filename: The name of the file
 
-create_task("test", "Este e um Teste1")
-create_task("test", "Este e um Teste2")
-create_task("test", "Este e um Teste3")
+def show_task(filename: str):
+    with open("data/" + filename + ".txt", "r") as f: 
+        lines = f.readlines()
+        for line in lines:
+            print(line)
 
-show_tasks("test")
+
+#This method Deletes tasks, it reads lines, then compares it with the task that will be deleted 
+#and if it is different it will write it again inside the file, arguments:
+# filename: The name of the file
+# task: the task the user wants to delete
+def delete_task(filename: str, task):
+    with open("data/" + filename + ".txt", "r+") as f: 
+        lines = f.readlines()
+        f.seek(0)
+        for line in lines:
+            if line.strip("\n") != task:
+                f.write(line)
+        f.truncate()
+
+#This method marks tasks as done, it reads lines, then compares it with the task that will be marked
+#and if it is equals it will mark it as done, arguments:
+# filename: The name of the file
+# task: the task the user wants to mark as done
+def done_task(filename: str, task):
+    with open("data/" + filename + ".txt", "r+") as f: 
+        lines = f.readlines()
+        f.seek(0)
+        for line in lines:
+            if line.strip("\n") == task:
+                line = line.strip("\n") + " --> Done" + "\n"
+            f.write(line)    
+        f.truncate()
+
+
+
